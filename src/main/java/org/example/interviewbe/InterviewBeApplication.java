13957e226a59bb7dd54b3e740d2fc7b45d70f2d2
BeCaBuCode <23520950@gm.uit.edu.vn>
2025-12-21 09:54:25 +0700

    feat: init project

---
+++ src/main/java/org/example/interviewbe/InterviewBeApplication.java
@@ -0,0 +1,13 @@
+package org.example.interviewbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InterviewBeApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InterviewBeApplication.class, args);
+    }
+
+}
